21fb48047d21c8223b7f893b61ce7c2eced265fd
Benjamin Barrientos <36490478+ZoheeDH@users.noreply.github.com>
2024-03-21 02:28:51 -0300

    E3.15-3.18

--- index.js
+++ index.js
@@ -18,30 +18,6 @@ app.use(morgan(
 ))
 app.use(express.static('dist'))
 
-
-// let persons = [
-//   { 
-//     "id": 1,
-//     "name": "Arto Hellas", 
-//     "number": "040-123456"
-//   },
-//   { 
-//     "id": 2,
-//     "name": "Ada Lovelace", 
-//     "number": "39-44-5323523"
-//   },
-//   { 
-//     "id": 3,
-//     "name": "Dan Abramov", 
-//     "number": "12-43-234345"
-//   },
-//   { 
-//     "id": 4,
-//     "name": "Mary Poppendieck", 
-//     "number": "39-23-6423122"
-//   }
-// ]
-
 app.get('/info', (req, res) => {
   const t = new Date(Date.now())
   Person.find({}).then(persons => 
@@ -57,18 +33,22 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  Person
-    .findById(req.params.id)
-    .then(person => res.json(person))
-    .catch(err => res.status(404).end())
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }})
+    .catch(err => next(err))
 })
 
-// app.delete('/api/persons/:id  ', (req, res) => {
-//   const id = Number(req.params.id)
-//   persons = persons.filter(person => person.id !== id)
-//   res.status(204).end()
-// })
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => res.status(204).end())
+    .catch(err => next(err))
+})
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
@@ -76,19 +56,43 @@ app.post('/api/persons', (req, res) => {
   if (!body.name || !body.number) {
     return res.status(400).send({error: 'Name or number missing'})
   }
-  // if (persons.find(person => person.name === body.name)) {
-  //   return res.status(400).send({error: 'Name must be unique'})
-  // }
+  Person.find({name: body.name})
+    .then(person => {
+      if (person.length > 0) {
+        res.status(400).send({error: 'Name must be unique'})
+      } else {
+        const person = new Person({
+          name: body.name, 
+          number: body.number
+        })
+        person.save().then(savedPerson => {
+          res.json(savedPerson)
+        })
+      }
+    })
+})
 
-  const person = new Person({
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+  const person = {
     name: body.name, 
     number: body.number
-  })
-  
-  person.save().then(savedPerson => {
-    res.json(savedPerson)
-  })
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(updatedPerson => res.json(updatedPerson))
+    .catch(err => next(err))
 })
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err.message)
+  if (err.name === 'CastError') {
+    return res.status(400).send({error: 'Malformatted id'})
+  }
+  next(err)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
